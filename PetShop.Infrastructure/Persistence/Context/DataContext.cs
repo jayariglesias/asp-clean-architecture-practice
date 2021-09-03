5ecdefca11dbb5944f4f5cf826d812af7e06bd6e
jeee <jayariglesias@gmail.com>
2021-09-03 18:26:36 +0800

    persistence

---
+++ PetShop.Infrastructure/Persistence/Context/DataContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetShop.Application.Common.Interfaces;
+using PetShop.Domain.Entities;
+
+namespace PetShop.Infrastructure.Persistence.Context
+{
+    public class DataContext : DbContext, IDataContext
+    {
+        // private readonly IDatetime _dateTime;
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<Visit> Visits { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("PetShop");
+
+            modelBuilder.Entity<Pet>()
+                .HasOne(pet => pet.User)
+                .WithMany(user => user.Pets)
+                .HasForeignKey(pet => pet.UserId)
+                .HasPrincipalKey(user => user.UserId);
+
+            modelBuilder.Entity<Visit>()
+                .HasOne(visit => visit.Pet)
+                .WithMany(pet => pet.Visits)
+                .HasForeignKey(visit => visit.PetId)
+                .HasPrincipalKey(pet => pet.PetId);
+        }
+    }
+}
