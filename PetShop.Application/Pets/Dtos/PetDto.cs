194906e2714b62c96ba41111d42ad97ccf162bd0
jeee <jayariglesias@gmail.com>
2021-09-07 21:42:16 +0800

    change wner to owner

--- PetShop.Application/Pets/Dtos/PetDto.cs
+++ PetShop.Application/Pets/Dtos/PetDto.cs
@@ -1,9 +1,11 @@
-﻿using Newtonsoft.Json;
+﻿using AutoMapper;
+using Newtonsoft.Json;
 using PetShop.Application.Common.Interfaces;
 using PetShop.Application.Common.Mappings;
 using PetShop.Domain.Entities;
 using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace PetShop.Application.Pets.Dtos
 {
@@ -14,15 +16,15 @@ namespace PetShop.Application.Pets.Dtos
             Visits = new List<VisitDto>();
         } 
 
-        public int UserId { get; set; }
         public int PetId { get; set; }
+        public int UserId { get; set; }
         public string PetName { get; set; }
         public DateTime Birthdate { get; set; }
         public int PetType { get; set; }
         public string Breed { get; set; }
         public IList<VisitDto> Visits { get; set; }
 
-        [JsonProperty(PropertyName = "owner")]
+        [JsonProperty(PropertyName = "Owner")]
         public UserDto User { get; set; }
     }
 }
