1e16f4881873a9de82740287d997c6cd97c47ce3
jeee <jayariglesias@gmail.com>
2021-09-29 00:48:49 +0800

    minor changes/updates

--- PetClinic.Application/Auth/Dtos/loginDto.cs
+++ PetClinic.Application/Auth/Dtos/loginDto.cs
@@ -6,17 +6,29 @@ namespace PetClinic.Application.Auth.Dtos
     {
         public loginDto(User user, string token)
         {
+            Token = token;
+            CurrentUser = new currentUser(user);
+        }
+
+        public currentUser CurrentUser { get; set; }
+        public string Token { get; set; }
+    }
+
+    public class currentUser
+    {
+        public currentUser(User user)
+        {
             UserId = user.UserId;
             FirstName = user.FirstName;
             LastName = user.LastName;
             Username = user.Username;
-            Token = token;
+            UserType = user.UserType;
         }
 
         public int UserId { get; set; }
+        public int UserType { get; set; }
+        public string Username { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Username { get; set; }
-        public string Token { get; set; }
     }
 }
